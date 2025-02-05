514d8b58ce3f25bc0d4ecbc6c72fb9a16edb2fda
JEldijk <j.eldijk@gmail.com>
2025-02-05 20:43:36 +0100

    update pyright configuration, add logging utility, and enhance hello script with logging features

--- apps/api-2/hello.py
+++ apps/api-2/hello.py
@@ -1,7 +1,20 @@
 import utils
+from utils.logging import init_logging, Format, LogLevels
+
+LOG = init_logging(format=Format.console, log_level=LogLevels.DEBUG)
+
 
 def main():
-    print(utils.hello())
+    LOG.info(utils.hello())
+
+    LOG.warning("warning message")
+
+    try:
+        raise Exception("Houston, we have a problem")
+    except Exception as e:
+        LOG.error(e, exc_info=True, stack_info=True)
+
+    LOG.debug("debug message after exception")
 
 
 if __name__ == "__main__":
--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,7 +1,9 @@
-import utils
 from fastapi import FastAPI
+from utils.logging import Format, LogLevels, init_logging
 
 from .config import CommonSettings
+
+LOG = init_logging(format=Format.console, log_level=LogLevels.DEBUG)
 
 app = FastAPI()
 
@@ -10,5 +12,6 @@ app = FastAPI()
 async def info(settings: CommonSettings):
     if not settings.admin_email:
         raise ValueError("admin_email is required")
-    utils.hello()
+    
+    LOG.error(f"admin_email: {settings.admin_email}")
     return settings.model_dump_json()
